696e64105d124236c9262b977720b512fa79c105
Michele Abbrescia <michele.abbrescia@lotrek.it>
2026-02-26 17:26:37 +0100

    chore: update pipe to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/auto-docs',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
